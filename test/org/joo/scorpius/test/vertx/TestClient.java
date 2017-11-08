bb7f1ba5adca51e60e3c77656508b821529ab438
griever <griever@PC01NH3V.sg.oracle.com>
2017-11-08 19:17:01 +0800

    add test HTTP client

---
+++ test/org/joo/scorpius/test/vertx/TestClient.java
@@ -0,0 +1,96 @@
+package org.joo.scorpius.test.vertx;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+		int maxThreads = 20;
+		int noConnections = 10000;
+		CloseableHttpAsyncClient httpClient = HttpAsyncClients.createDefault();
+		httpClient.start();
+		ExecutorService executor = Executors.newFixedThreadPool(maxThreads);
+		HttpPost request = new HttpPost("http://localhost:8080/msg?name=greet");
+		String xml = "{\"name\": \"Anh Dung\"}";
+        HttpEntity entity = new ByteArrayEntity(xml.getBytes());
+        request.setEntity(entity);
+        
+        CountDownLatch latch = new CountDownLatch(1);
+        
+        Handler handler = new Handler(latch, noConnections);
+        
+		long start = System.currentTimeMillis();
+		
+		for(int i=0; i<noConnections; i++) {
+			executor.submit(() -> {
+				httpClient.execute(request, handler);
+			});
+		}
+		
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		long elapsed = System.currentTimeMillis() - start;
+		long pace = noConnections * 1000 / elapsed;
+		
+		System.out.println("Elapsed: " + elapsed + "ms");
+		System.out.println("Pace: " + pace + " connections/sec");
+		System.out.println("Success: " + handler.getCounter());
+		System.out.println("Failed: " + handler.getFailed());
+
+		executor.shutdown();
+	}
+}
+
+class Handler implements FutureCallback<HttpResponse> {
+
+	private int counter = 0;
+	
+	private int failed = 0;
+
+	private int noConnections = 0;
+
+	private CountDownLatch latch;
+
+	public Handler(CountDownLatch latch, int noConnections) {
+		this.latch = latch;
+		this.noConnections = noConnections;
+	}
+
+	@Override
+	public void completed(HttpResponse result) {
+		counter++;
+		if (counter == noConnections)
+			latch.countDown();
+	}
+
+	@Override
+	public void failed(Exception ex) {
+		failed++;
+	}
+
+	@Override
+	public void cancelled() {
+	}
+	
+	public int getCounter() {
+		return counter;
+	}
+	
+	public int getFailed() {
+		return failed;
+	}
+}
