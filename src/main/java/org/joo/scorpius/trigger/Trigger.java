2c3a39790019fe9135aaab10d4304ee5716537e1
griever <griever@PC01NH3V.sg.oracle.com>
2017-11-16 13:57:31 +0800

    add trigger name

--- src/main/java/org/joo/scorpius/trigger/Trigger.java
+++ src/main/java/org/joo/scorpius/trigger/Trigger.java
@@ -5,6 +5,10 @@ import org.joo.scorpius.support.BaseResponse;
 import org.joo.scorpius.support.TriggerExecutionException;
 
 public interface Trigger<T extends BaseRequest, H extends BaseResponse> {
-
+	
 	public void execute(TriggerExecutionContext executionContext) throws TriggerExecutionException;
+
+	default public String getName() {
+		return getClass().getName();
+	}
 }
