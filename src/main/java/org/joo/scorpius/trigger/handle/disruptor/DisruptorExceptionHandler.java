f058d2412fcc844dea131315b392a074049b88f6
griever <griever@PC01NH3V.sg.oracle.com>
2017-11-16 13:48:47 +0800

    don't call executionContext.fail here

--- src/main/java/org/joo/scorpius/trigger/handle/disruptor/DisruptorExceptionHandler.java
+++ src/main/java/org/joo/scorpius/trigger/handle/disruptor/DisruptorExceptionHandler.java
@@ -1,18 +1,15 @@
 package org.joo.scorpius.trigger.handle.disruptor;
 
-import org.joo.scorpius.support.TriggerExecutionException;
-import org.joo.scorpius.trigger.TriggerExecutionContext;
-
 import com.lmax.disruptor.ExceptionHandler;
 
 public class DisruptorExceptionHandler implements ExceptionHandler {
-
+	
 	@Override
 	public void handleEventException(Throwable ex, long sequence, Object event) {
 		if (event == null || !(event instanceof ExecutionContextEvent))
 			return;
-		TriggerExecutionContext executionContext = ((ExecutionContextEvent)event).getExecutionContext();
-		executionContext.fail(new TriggerExecutionException(ex));
+
+		//TODO log
 	}
 
 	@Override
