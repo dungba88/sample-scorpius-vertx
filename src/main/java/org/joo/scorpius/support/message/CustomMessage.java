0475b8efca2500cd4e4544b5aee8c5eaf92396da
Dzung Bui <dungba.sg@gmail.com>
2017-11-17 15:17:07 +0800

    support custom trigger message

---
+++ src/main/java/org/joo/scorpius/support/message/CustomMessage.java
@@ -0,0 +1,25 @@
+package org.joo.scorpius.support.message;
+
+import java.io.Serializable;
+
+public class CustomMessage implements Serializable {
+
+	private static final long serialVersionUID = 6888189228534475717L;
+
+	private final String name;
+	
+	private final Serializable customObject;
+	
+	public CustomMessage(String name, Serializable customObject) {
+		this.name = name;
+		this.customObject = customObject;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Serializable getCustomObject() {
+		return customObject;
+	}
+}
