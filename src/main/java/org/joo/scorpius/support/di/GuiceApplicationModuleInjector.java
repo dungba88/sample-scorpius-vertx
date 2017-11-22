5c0a39205591661ce9c455ba55bada88d2e52d77
Dzung Bui <dungba.sg@gmail.com>
2017-11-22 22:36:46 +0800

    optimize

--- src/main/java/org/joo/scorpius/support/di/GuiceApplicationModuleInjector.java
+++ src/main/java/org/joo/scorpius/support/di/GuiceApplicationModuleInjector.java
@@ -1,5 +1,8 @@
 package org.joo.scorpius.support.di;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
@@ -8,13 +11,37 @@ public class GuiceApplicationModuleInjector implements ApplicationModuleInjector
 
 	private Injector injector;
 	
+	private ThreadLocal<Map<Class<?>, Object>> instancesMap;
+	
+	private boolean cache;
+	
 	public GuiceApplicationModuleInjector(Module... modules) {
-		this.injector = Guice.createInjector(modules);
+		this(true, modules);
 	}
 	
+	public GuiceApplicationModuleInjector(boolean cache, Module... modules) {
+		this.cache = cache;
+		this.injector = Guice.createInjector(modules);
+		this.instancesMap = new ThreadLocal<Map<Class<?>, Object>>() {
+			@Override public Map<Class<?>, Object> initialValue() {
+	            return new HashMap<>();
+	        }
+		};
+	}
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public <T> T getInstance(Class<T> clazz) {
-		return injector.getInstance(clazz);
+		if (!cache)
+			return injector.getInstance(clazz);
+
+		Map<Class<?>, Object> map = instancesMap.get();
+		T instance = (T) map.get(clazz);
+		if (instance != null) return instance;
+		
+		instance = injector.getInstance(clazz);
+		map.put(clazz, instance);
+		return instance;
 	}
 	
 	public Injector getInternalInjector() {
