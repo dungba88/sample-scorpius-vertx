66da5b615ffc9ec95717a4a2d1feeaeb6cd275c2
Dzung Bui <dungba.sg@gmail.com>
2017-11-23 20:29:51 +0800

    refactor

--- src/test/java/org/joo/scorpius/test/perf/DefaultNonDeferredTriggerHandlingTest.java
+++ src/test/java/org/joo/scorpius/test/perf/DefaultNonDeferredTriggerHandlingTest.java
@@ -16,11 +16,6 @@ public class DefaultNonDeferredTriggerHandlingTest extends AbstractTriggerTest {
 	}
 
 	@Override
-	protected void cleanup() {
-		
-	}
-
-	@Override
 	protected void doTest(long iterations, String msgName) {
 		processed = 0;
 		CountDownLatch latch = new CountDownLatch(1);
