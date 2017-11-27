5a9e21d4f7e05997354d89f61b8d3c64b56f0cee
griever <griever@PC01NH3V.sg.oracle.com>
2017-11-27 18:36:03 +0800

    refactor

--- src/main/java/org/joo/scorpius/support/builders/id/TimeBasedIdGenerator.java
+++ src/main/java/org/joo/scorpius/support/builders/id/TimeBasedIdGenerator.java
@@ -5,11 +5,11 @@ import java.util.Optional;
 import org.joo.scorpius.support.builders.contracts.IdGenerator;
 
 import com.fasterxml.uuid.Generators;
-import com.fasterxml.uuid.impl.TimeBasedGenerator;
+import com.fasterxml.uuid.NoArgGenerator;
 
 public class TimeBasedIdGenerator implements IdGenerator {
 
-    private TimeBasedGenerator generator;
+    private NoArgGenerator generator;
 
     public TimeBasedIdGenerator() {
         generator = Generators.timeBasedGenerator();
