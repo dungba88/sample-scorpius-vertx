99a80e0c6b7279e00a963a3e593e5941ac53dc7c
griever <griever@PC01NH3V.sg.oracle.com>
2017-11-27 18:37:01 +0800

    add naming prefix

--- src/main/java/org/joo/scorpius/support/builders/DefaultThreadFactoryBuilder.java
+++ src/main/java/org/joo/scorpius/support/builders/DefaultThreadFactoryBuilder.java
@@ -1,29 +1,52 @@
 package org.joo.scorpius.support.builders;
 
-import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.joo.scorpius.support.builders.contracts.TriggerThreadFactory;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NonNull;
 import lombok.experimental.Accessors;
 
-@Getter
-@Setter
 @Accessors(chain = true)
-public class DefaultThreadFactoryBuilder implements Builder<ThreadFactory> {
+public class DefaultThreadFactoryBuilder implements Builder<TriggerThreadFactory> {
 
-    private boolean daemon;
+    private @Getter boolean daemon = false;
 
-    private int priority = Thread.NORM_PRIORITY;
+    private @Getter int priority = Thread.NORM_PRIORITY;
+
+    private @Getter String prefix = "trigger-worker-";
+
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    public DefaultThreadFactoryBuilder() {
+
+    }
+
+    public DefaultThreadFactoryBuilder(final int priority, final boolean daemon, final @NonNull String prefix) {
+        this.priority = priority;
+        this.daemon = daemon;
+        this.prefix = prefix;
+    }
 
     @Override
-    public ThreadFactory build() {
-        return runnable -> {
-            Thread t = new Thread(runnable);
-            if (t.isDaemon() != daemon)
-                t.setDaemon(daemon);
-            if (t.getPriority() != priority)
-                t.setPriority(priority);
-            return t;
+    public TriggerThreadFactory build() {
+        return new TriggerThreadFactory() {
+
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r, prefix + counter.getAndIncrement());
+                if (t.isDaemon() != daemon)
+                    t.setDaemon(daemon);
+                if (t.getPriority() != priority)
+                    t.setPriority(priority);
+                return t;
+            }
+
+            @Override
+            public boolean isConsumerThread(Thread thread) {
+                return thread.getName().startsWith(prefix);
+            }
         };
     }
 }
