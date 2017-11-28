77effccaac895b427fcac5eeee552d62a5e1c277
griever <griever@PC01NH3V.sg.oracle.com>
2017-11-28 13:23:56 +0800

    add test case

---
+++ src/test/java/org/joo/scorpius/test/perf/ExecutorStealingTriggerHandlingTest.java
@@ -0,0 +1,41 @@
+package org.joo.scorpius.test.perf;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.joo.scorpius.test.support.SampleRequest;
+import org.joo.scorpius.trigger.handle.ExecutorHandlingStrategy;
+import org.junit.Assert;
+
+public class ExecutorStealingTriggerHandlingTest extends AbstractTriggerTest {
+
+    private AtomicInteger processed = new AtomicInteger(0);
+
+    public ExecutorStealingTriggerHandlingTest(long iterations) {
+        super(iterations, new ExecutorHandlingStrategy(Executors.newWorkStealingPool()));
+    }
+
+    @Override
+    protected void doTest(long iterations, String msgName) {
+        processed = new AtomicInteger(0);
+        CountDownLatch latch = new CountDownLatch(1);
+
+        for (int i = 0; i < iterations; i++) {
+            manager.fire(msgName, new SampleRequest("World")).done(response -> {
+                if (processed.incrementAndGet() == iterations) {
+                    latch.countDown();
+                }
+            });
+        }
+
+        try {
+            latch.await(10000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(processed.get() == iterations);
+    }
+}
