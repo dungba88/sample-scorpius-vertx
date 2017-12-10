0050168396081e4a6cd7b8a8e4312bb529152743
griever <griever@dungs-mbp>
2017-12-10 20:27:11 +0800

    support passing of external routing configuration

--- scorpius-vertx/src/main/java/org/joo/scorpius/support/vertx/VertxBootstrap.java
+++ scorpius-vertx/src/main/java/org/joo/scorpius/support/vertx/VertxBootstrap.java
@@ -1,5 +1,7 @@
 package org.joo.scorpius.support.vertx;
 
+import java.util.function.Function;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.joo.scorpius.support.bootstrap.AbstractBootstrap;
@@ -13,53 +15,64 @@ import io.vertx.ext.web.handler.BodyHandler;
 
 public class VertxBootstrap extends AbstractBootstrap {
 
-    private static final Logger logger = LogManager.getLogger(VertxBootstrap.class);
+	private static final Logger logger = LogManager.getLogger(VertxBootstrap.class);
 
-    private static final String DEFAULT_ENDPOINT = "/msg";
+	private static final String DEFAULT_ENDPOINT = "/msg";
 
-    private VertxMessageController msgController;
+	private VertxMessageController msgController;
 
-    private Vertx vertx;
+	private Vertx vertx;
 
-    private HttpServer server;
+	private HttpServer server;
 
-    private String endpoint;
+	private String endpoint;
 
-    private int port;
+	private int port;
 
-    public VertxBootstrap(final VertxOptions vertxOptions, final int port) {
-        this(vertxOptions, new HttpServerOptions(), port);
-    }
+	private Function<Vertx, Router> routingConfig;
 
-    public VertxBootstrap(final VertxOptions vertxOptions, final HttpServerOptions httpOptions, final int port) {
-        this(vertxOptions, httpOptions, port, DEFAULT_ENDPOINT);
-    }
+	public VertxBootstrap(final VertxOptions vertxOptions, final int port) {
+		this(vertxOptions, new HttpServerOptions(), port);
+	}
 
-    public VertxBootstrap(final VertxOptions vertxOptions, final HttpServerOptions httpOptions, final int port,
-            String endpoint) {
-        this.vertx = Vertx.vertx(vertxOptions);
-        this.server = vertx.createHttpServer(httpOptions);
-        this.port = port;
-        this.endpoint = endpoint;
-    }
+	public VertxBootstrap(final VertxOptions vertxOptions, final HttpServerOptions httpOptions, final int port) {
+		this(vertxOptions, httpOptions, port, DEFAULT_ENDPOINT);
+	}
 
-    public void run() {
-        msgController = new VertxMessageController(triggerManager);
-        Router restAPI = configureRoutes(vertx);
-        server.requestHandler(restAPI::accept).listen(port, res -> {
-            if (res.failed()) {
-                if (logger.isFatalEnabled())
-                    logger.fatal("Exception occurred while initializing Vertx Web", res.cause());
-            } else if (logger.isInfoEnabled()) {
-                logger.info("Vertx Web started listening at port " + port);
-            }
-        });
-    }
+	public VertxBootstrap(final VertxOptions vertxOptions, final HttpServerOptions httpOptions, final int port,
+			String endpoint) {
+		this.vertx = Vertx.vertx(vertxOptions);
+		this.server = vertx.createHttpServer(httpOptions);
+		this.port = port;
+		this.endpoint = endpoint;
+	}
 
-    protected Router configureRoutes(final Vertx vertx) {
-        Router restAPI = Router.router(vertx);
-        restAPI.post("/*").handler(BodyHandler.create());
-        restAPI.post(endpoint).handler(msgController::handle);
-        return restAPI;
-    }
+	public VertxBootstrap(final VertxOptions vertxOptions, final HttpServerOptions httpOptions, final int port,
+			Function<Vertx, Router> routingConfig) {
+		this.vertx = Vertx.vertx(vertxOptions);
+		this.server = vertx.createHttpServer(httpOptions);
+		this.port = port;
+		this.routingConfig = routingConfig;
+	}
+
+	public void run() {
+		msgController = new VertxMessageController(triggerManager);
+		Router restAPI = routingConfig != null ? routingConfig.apply(vertx) : configureRoutes(vertx);
+		server.requestHandler(restAPI::accept).listen(port, res -> {
+			if (res.failed()) {
+				if (logger.isFatalEnabled())
+					logger.fatal("Exception occurred while initializing Vertx Web", res.cause());
+				server.close();
+			} else if (logger.isInfoEnabled()) {
+				logger.info("Vertx Web started listening at port " + port);
+			}
+		});
+	}
+
+	protected Router configureRoutes(final Vertx vertx) {
+		Router restAPI = Router.router(vertx);
+		restAPI.post("/*").handler(BodyHandler.create());
+		restAPI.post(endpoint).handler(msgController::handle);
+		return restAPI;
+	}
 }
