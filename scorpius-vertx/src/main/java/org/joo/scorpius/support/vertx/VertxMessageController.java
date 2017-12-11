4b6eb076e0c256e40e5368d833eca5e647ff7492
griever <griever@PC01NH3V.sg.oracle.com>
2017-12-11 14:40:53 +0800

    refactor

--- scorpius-vertx/src/main/java/org/joo/scorpius/support/vertx/VertxMessageController.java
+++ scorpius-vertx/src/main/java/org/joo/scorpius/support/vertx/VertxMessageController.java
@@ -9,29 +9,31 @@ import io.vertx.ext.web.RoutingContext;
 
 public class VertxMessageController extends AbstractVertxController {
 
-	public VertxMessageController(final TriggerManager triggerManager) {
-		super(triggerManager);
-	}
+    public VertxMessageController(final TriggerManager triggerManager) {
+        super(triggerManager);
+    }
 
-	public void handle(final RoutingContext rc) {
-		HttpServerResponse response = rc.response();
-		response.putHeader("Content-Type", "application/json");
+    public void handle(final RoutingContext rc) {
+        HttpServerResponse response = rc.response();
+        response.putHeader("Content-Type", "application/json");
 
-		String msgName = rc.request().getParam("name");
-		if (msgName == null)
-		    throw new IllegalArgumentException("Name cannot be null");
-		
-		String msgData = rc.getBodyAsString();
+        String msgName = rc.request().getParam("name");
+        if (msgName == null) {
+            rc.fail(new MalformedRequestException("Name cannot be null"));
+            return;
+        }
 
-		BaseRequest request = null;
+        String msgData = rc.getBodyAsString();
 
-		try {
-			request = triggerManager.decodeRequestForEvent(msgName, msgData);
-		} catch (MalformedRequestException e) {
-			onFail(e, response, rc);
-			return;
-		}
-		
-		doFireEvent(rc, msgName, request);
-	}
+        BaseRequest request = null;
+
+        try {
+            request = triggerManager.decodeRequestForEvent(msgName, msgData);
+        } catch (MalformedRequestException e) {
+            onFail(e, response, rc);
+            return;
+        }
+
+        doFireEvent(rc, msgName, request);
+    }
 }
