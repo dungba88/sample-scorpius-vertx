6102dd32e9fd05593eff649c1d63d78cc18440ac
griever <griever@dungs-mbp>
2017-12-12 01:13:01 +0800

    change list to array

--- scorpius-core/src/main/java/org/joo/scorpius/trigger/TriggerRepository.java
+++ scorpius-core/src/main/java/org/joo/scorpius/trigger/TriggerRepository.java
@@ -1,12 +1,10 @@
 package org.joo.scorpius.trigger;
 
-import java.util.List;
-
 import org.joo.scorpius.support.LifeCycle;
 
 public interface TriggerRepository extends LifeCycle {
 
     public TriggerRegistration registerTrigger(final String name, final TriggerConfig triggerConfig);
     
-    public List<TriggerConfig> getTriggerConfigs(String name);
+    public TriggerConfig[] getTriggerConfigs(String name);
 }
